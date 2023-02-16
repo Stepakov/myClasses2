c84ab7d12e4f01f6dcf8b8a235727a914b06d771
Sashka <sanechkastepakov@gmail.com>
2023-02-17 00:05:21 +0200

    Gender

--- classes/Gender/Gender.php
+++ classes/Gender/Gender.php
@@ -6,4 +6,12 @@ class Gender
 {
     public const MALE = 1;
     public const FEMALE = 2;
+
+    public static function getGenderList() : array
+    {
+        return [
+            'MALE' => self::MALE,
+            'FEMALE' => self::FEMALE
+        ];
+    }
 }
